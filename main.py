ceb8dd7b3eb7e0d2e08249d4f832ab47f6e70e91
Tom de Paula <thomasdp.barbosa@gmail.com>
2025-05-14 01:20:29 +0200

    add main program file

---
+++ main.py
@@ -0,0 +1,56 @@
+from playsound3 import playsound
+from pathlib import Path
+from time import sleep
+import random
+
+AUDIO_FOLDER = Path("audio/")
+DEBUG = True
+
+
+class Door:
+    def __init__(self, state: str = "closed") -> None:
+        self.state = state
+
+    def switch_state(self, new_state) -> None:
+        if new_state == "open":
+            self.state = "open"
+        elif new_state == "closed":
+            self.state = "closed"
+        else:
+            raise RuntimeError("Somehow we got a nonexistent state.")
+
+
+def get_current_state(*, debug: bool = False) -> str:
+    if debug:
+        # NOTE: the magnets "are touching" if the file exists
+        magnet_file = Path("magnet")
+        magnets_are_connected: bool = magnet_file.exists()
+    else:
+        # TODO: substitute with actual implementation of magnet library
+        raise NotImplementedError("Not yet implemented")
+
+    if magnets_are_connected:
+        return "closed"
+    else:
+        return "open"
+
+
+def main():
+    sound_library = [s for s in AUDIO_FOLDER.iterdir()]
+    door = Door(get_current_state())
+    while True:
+        new_state = get_current_state(debug=DEBUG)
+
+        if door.state != new_state:
+            door.switch_state(new_state)
+
+            # door just opened
+            if new_state == "open":
+                chosen_sound = random.choice(sound_library)
+                playsound(chosen_sound)
+
+        sleep(1)  # seconds
+
+
+if __name__ == "__main__":
+    main()
